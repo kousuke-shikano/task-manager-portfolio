68568bde5a7f22f67f4ce85c5908260efa258616
kousuke-shikano <kosukeshikano@gmail.com>
2025-10-02 23:39:30 +0900

    ルート設定

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // 認証済みならタスク一覧にリダイレクト
+    return redirect()->route('tasks.index');
 });
 
 Route::get('/dashboard', function () {
